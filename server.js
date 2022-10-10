7e82a0925e2aba23757649b43a8d857142d1d138
developerHet <hetpatel2312.hp@gmail.com>
2022-10-10 16:56:04 +0530

    Setup routes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const dontenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+//Load env vars
 dontenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
+
+
 
 const PORT = process.env.PORT || 5000;
 
