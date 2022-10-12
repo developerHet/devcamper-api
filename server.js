95f319b55ad13bbf96b8e545b75419f35f914896
developerHet <hetpatel2312.hp@gmail.com>
2022-10-12 18:55:50 +0530

    Setup colors library.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const dontenv = require("dotenv");
-const morgan = require("morgan");
+const express = require('express');
+const dontenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require("./config/db");
 
 // Route files
@@ -26,14 +27,14 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
 
 //Handle unhandled promise rejections
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //Close server and exit process
   server.close(() => process.exit(1));
 });
