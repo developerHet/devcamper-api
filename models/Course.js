30cf9e4ea8e196fd2c3ea3250cd7cb9f20e40818
developerHet <hetpatel2312.hp@gmail.com>
2022-11-23 18:52:24 +0530

    Implement Update  Delete Course

--- models/Course.js
+++ models/Course.js
@@ -21,7 +21,7 @@ const CourseSchema = new mongoose.Schema({
     minimumSkill: {
         type: String,
         required: [true, 'Please add a minimum skill'],
-        enum: ['beginner','intermediate','adavanced']
+        enum: ['beginner','intermediate','advanced']
     },
     scholarshipAvailable: {
         type: Boolean,
