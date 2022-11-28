fbe5d75abebc77e605721bfa75ae884a9b89e3cf
developerHet <hetpatel2312.hp@gmail.com>
2022-11-28 19:39:59 +0530

    Implement Course User Relationship & Ownership

--- controllers/courses.js
+++ controllers/courses.js
@@ -47,12 +47,23 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access      private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`),
       404
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+        401
+      )
     );
   }
 
@@ -75,6 +86,16 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
       404
     );
   }
+
+    // Make sure user is bootcamp owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return next(
+        new ErrorResponse(
+          `User ${req.user.id} is not authorized to update course ${course._id}`,
+          401
+        )
+      );
+    }
 
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -99,6 +120,16 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
       404
     );
   }
+
+      // Make sure user is bootcamp owner
+      if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+          new ErrorResponse(
+            `User ${req.user.id} is not authorized to delete course ${course._id}`,
+            401
+          )
+        );
+      }
 
   await course.remove();
 
