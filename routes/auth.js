3fe3b97ca2c7679255097a496a6c897da5bb30a8
developerHet <hetpatel2312.hp@gmail.com>
2022-11-29 19:19:56 +0530

    Forgot Password - Generate Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const { register, login, getMe,forgotpassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -7,4 +7,6 @@ const router = express.Router();
 router.post('/register',register);
 router.post('/login',login);
 router.get('/me',protect,getMe);
+router.post('/forgotpassword',forgotpassword);
+
 module.exports = router;
