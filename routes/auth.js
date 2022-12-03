22a1c0a7604c1f175eb9eeda96975446f4e574e5
developerHet <hetpatel2312.hp@gmail.com>
2022-12-03 15:08:59 +0530

    Implement update user details

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const { register, login, getMe,forgotpassword, resetPassword } = require('../controllers/auth');
-const { protect } = require('../middleware/auth');
+const express = require("express");
+const {
+  register,
+  login,
+  getMe,
+  forgotpassword,
+  resetPassword,
+  updateDetails,
+  updatePassword
+} = require("../controllers/auth");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post('/register',register);
-router.post('/login',login);
-router.get('/me',protect,getMe);
-router.post('/forgotpassword',forgotpassword);
-router.put('/resetpassword/:resettoken',resetPassword);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
+router.post("/forgotpassword", forgotpassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
